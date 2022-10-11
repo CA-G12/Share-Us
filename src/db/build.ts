0263111f775a685dec011fb072c09dcd10a13434
shamskhodary <shams.khodary@gmail.com>
2022-10-11 14:10:13 +0300

    make changes in the build relates #3

--- src/db/build.ts
+++ src/db/build.ts
@@ -1,24 +1,7 @@
-import { sequelize, User, Event, JoinedPeople, InterestedPeople, Comments, Hashtag, HashtagEvent, Chat } from '.'
+import { sequelize } from '.'
 
 const build = async () => {
   await sequelize.sync({ force: true })
 }
-User.hasMany(Event)
-Event.belongsTo(User)
-
-User.belongsToMany(Event, { through: JoinedPeople })
-Event.belongsToMany(User, { through: JoinedPeople })
-
-User.belongsToMany(Event, { through: InterestedPeople })
-Event.belongsToMany(User, { through: InterestedPeople })
-
-User.hasMany(Comments)
-Comments.belongsTo(User)
-
-Hashtag.belongsToMany(Event, { through: HashtagEvent })
-Event.belongsToMany(Hashtag, { through: HashtagEvent })
-
-Chat.belongsTo(User, { as: 'receiver' })
-Chat.belongsTo(User, { as: 'sender' })
 
 build()
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,21 @@
 import sequelize from './connection'
 import { Event, User, JoinedPeople, InterestedPeople, Comments, Hashtag, HashtagEvent, Chat } from '../models'
+
+User.hasMany(Event)
+Event.belongsTo(User)
+
+User.belongsToMany(Event, { through: JoinedPeople })
+Event.belongsToMany(User, { through: JoinedPeople })
+
+User.belongsToMany(Event, { through: InterestedPeople })
+Event.belongsToMany(User, { through: InterestedPeople })
+
+User.hasMany(Comments)
+Comments.belongsTo(User)
+
+Hashtag.belongsToMany(Event, { through: HashtagEvent })
+Event.belongsToMany(Hashtag, { through: HashtagEvent })
+
+Chat.belongsTo(User, { as: 'receiver' })
+Chat.belongsTo(User, { as: 'sender' })
 export { sequelize, User, Event, JoinedPeople, InterestedPeople, Comments, Hashtag, HashtagEvent, Chat }
