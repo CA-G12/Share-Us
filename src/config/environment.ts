317fca488555656845d21357499528b32c1d2d85
shamskhodary <shams.khodary@gmail.com>
2022-10-11 23:13:15 +0300

    added the production, test environments, in addition to the secret key to the config object relates #9

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,12 +2,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const { NODE_ENV, PORT, DB_URL } = process.env
+const { NODE_ENV, PORT, DB_URL, DATABASE_URL, TEST_DB, SECRET_KEY } = process.env
 
 const config = {
   nodeEnv: NODE_ENV || 'development',
   port: PORT || 8080,
-  db: DB_URL || ''
+  db: DB_URL || '',
+  db_production: DATABASE_URL || '',
+  db_test: TEST_DB || '',
+  secret_key: SECRET_KEY
 }
 
 export default config
