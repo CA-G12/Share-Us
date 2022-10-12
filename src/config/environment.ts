50ce9e6cae1738a46b9232318312837cccbfc426
shamskhodary <shams.khodary@gmail.com>
2022-10-12 10:51:13 +0300

    moved the connection cases into the environment file, then fixed the build and start scripts relates #9

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,13 +4,32 @@ dotenv.config()
 
 const { NODE_ENV, PORT, DB_URL, DATABASE_URL, TEST_DB, SECRET_KEY } = process.env
 
+let connectionString: string | undefined = ''
+let ssl: boolean | object = false
+
+if (NODE_ENV === 'development') {
+  connectionString = DB_URL
+} else if (NODE_ENV === 'production') {
+  connectionString = DATABASE_URL
+  ssl = {
+    rejectUnauthorized: false
+  }
+} else if (NODE_ENV === 'test') {
+  connectionString = TEST_DB
+} else {
+  throw new Error('Invalid Node Env')
+}
+
+if (!connectionString) {
+  throw new Error('Database url is not valid')
+}
+
 const config = {
-  nodeEnv: NODE_ENV || 'development',
+  nodeEnv: NODE_ENV,
   port: PORT || 8080,
-  db: DB_URL || '',
-  db_production: DATABASE_URL || '',
-  db_test: TEST_DB || '',
-  secret_key: SECRET_KEY
+  connectionString,
+  ssl,
+  secretKey: SECRET_KEY
 }
 
 export default config
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,22 +1,5 @@
 import { Sequelize } from 'sequelize'
 import config from '../config/environment'
-
-let connectionString: string = ''
-let ssl: boolean | object = false
-
-if (config.nodeEnv === 'development') {
-  connectionString = config.db
-} else if (config.nodeEnv === 'production') {
-  connectionString = config.db_production
-  ssl = {
-    rejectUnauthorized: false
-  }
-} else if (config.nodeEnv === 'test') {
-  connectionString = config.db_test
-} else {
-  throw new Error('Database url is not valid')
-}
-
-const sequelize = new Sequelize(connectionString, { dialectOptions: { ssl } })
+const sequelize = new Sequelize(config.connectionString, { dialectOptions: { ssl: config.ssl } })
 
 export default sequelize
