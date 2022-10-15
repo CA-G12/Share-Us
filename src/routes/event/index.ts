d2fd4c866a84b5ac96725625929c9288d177b7d2
Bakeza Diazada <dbakeza2002@gmail.com>
2022-10-15 22:35:07 +0300

    forget to push index file :)

--- src/routes/event/index.ts
+++ src/routes/event/index.ts
@@ -3,5 +3,6 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/events', EventsController.store)
+router.get('/events', EventsController.index)
 
 export default router
