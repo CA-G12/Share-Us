15416d126e10baa901088f5ab09273aa8961bfbd
Bakeza Diazada <dbakeza2002@gmail.com>
2022-10-22 18:54:16 +0300

    add app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import router from './routes'
 import config from './config/environment'
-import cors from 'cors'
 
 class App {
   public app: Application
@@ -21,11 +20,7 @@ class App {
     this.app.use(express.json())
     this.app.use(cookieParser())
     this.app.use(express.urlencoded({ extended: false }))
-<<<<<<< HEAD
-    this.app.use(cors());
-=======
     this.app.use(cors())
->>>>>>> 69bcc0d2877ece09cbab77ac47bf7fbc7a79ca8d
     this.app.use('/api/v1', router)
     this.app.use((err: any, req: Request, res: Response, next: NextFunction) => {
       res.status(err.status).json({ message: err.message })
