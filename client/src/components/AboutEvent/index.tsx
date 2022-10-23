23badaa9459e36fc2f989eb5b87c177ff8cbb607
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-10-23 12:01:15 +0300

    create AboutEvent Component to display event description , and contains Map component and JoinedPeople component relates #57

--- client/src/components/AboutEvent/index.tsx
+++ client/src/components/AboutEvent/index.tsx
@@ -1,57 +1,67 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-undef */
 import './style.css'
-import { FC } from 'react'
+import { useState, FC } from 'react'
 import { Paper } from '@mui/material'
-import mapImg from '../../assets/images/map.png'
+import Map from '../Map'
+import JoinedPeopleModel from '../JoinedPeopleModel.tsx'
+import IPropsAboutEvent from '../../interfaces/props/AboutEvent'
+// eslint-disable-next-line no-undef
+const AboutEvent:FC < IPropsAboutEvent > = ({
+  description, Hashtags, joinedPeople, interestedPeople, longitude, latitude,
+}): JSX.Element => {
+  console.log(Hashtags)
+  const [open, setOpen] = useState<boolean>(false)
+  const handleOpen = ():void => setOpen(true)
+  const handleClose = ():void => setOpen(false)
 
-const AboutEvent:FC = () => (
-  <div className="about-event">
-    <div className="description-container">
-      <h3>description</h3>
-      <p>
-        It’s almost time to trade in your COVID-safe mask for one that’s,
-        well, a little more fun.  From the costumes to the candy, Halloween
-        is a low-stress holiday event that allows team members to interact
-        in a laid-back setting as well as showcase their creativity.
-      </p>
-      <div className="hashtag-container">
-        <Paper
-          elevation={1}
-          variant="outlined"
-          className="hashtag-label"
-        >
-          #hashtag
-        </Paper>
-        <Paper
-          elevation={1}
-          variant="outlined"
-          className="hashtag-label"
-        >
-          #hashtag2
-        </Paper>
-
-      </div>
-    </div>
-
-    <div className="participants-map">
-      <div className="participants-container">
-        <h3>Participants</h3>
-        <div>
-          <p>
-            <span>150</span>
-            interested
-          </p>
-          <p>
-            <span>42</span>
-            Joined
-          </p>
+  return (
+    <div className="about-event">
+      <div className="description-container">
+        <h3>description</h3>
+        <p>
+          {description}
+        </p>
+        <div className="hashtag-container">
+          {
+        Hashtags
+        && Hashtags.map((ele: any) => (
+          <Paper
+            key={ele.id}
+            elevation={1}
+            variant="outlined"
+            className="hashtag-label"
+            sx={{ backgroundColor: ele.color }}
+          >
+            {ele.title}
+          </Paper>
+        ))
+      }
         </div>
       </div>
-      <div className="map-container">
-        <img className="map" src={mapImg} alt="map" />
-        <p>Location: London</p>
+
+      <div className="participants-map">
+        <div className="participants-container">
+          <h3>Participants</h3>
+          <div>
+            <button>
+              <span>{interestedPeople.length}</span>
+              interested
+            </button>
+            <button onClick={handleOpen}>
+              <span>{joinedPeople.length}</span>
+              Joined
+            </button>
+          </div>
+        </div>
+        <div className="map-container">
+          <Map longitude={longitude} latitude={latitude} />
+          <p>Location: London</p>
+        </div>
       </div>
+      <JoinedPeopleModel joinedPeople={joinedPeople} handleClose={handleClose} open={open} />
     </div>
-  </div>
-)
+  )
+}
 
 export default AboutEvent
