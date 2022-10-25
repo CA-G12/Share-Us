53d43c465c05f088bd4580095692c61ab7d65fa6
MostafaBalousha123 <mbalousha2001@gmail.com>
2022-10-25 08:27:24 +0300

    saif made it before ,I create it just for test results relates #57

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -28,6 +28,23 @@ Router.get('/events/:eventId/comments', expressWrapper(async (req: Request, res:
   res.json({ message: 'Comments received successfully', data: allComments })
 }))
 
+Router.post('/events/:eventId/comments'
+  , expressWrapper(async (req: Request, res: Response) => {
+    const { eventId } = req.params
+    const { image, content } = req.body
+    const comments = await Comments.create(
+      { image, UserId: 1, EventId: eventId, content }
+    )
+    const commentsAndPublisher = await Comments.findOne({
+      where: { UserId: 1 },
+      include: {
+        model: User,
+        attributes: ['id', 'username', 'profileImg']
+      }
+    })
+    res.json({ message: 'Comment add successfully', data: commentsAndPublisher })
+  }))
+
 Router.use(eventsRouter)
 
 export default Router
