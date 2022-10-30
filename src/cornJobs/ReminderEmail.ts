6eb38a1199b0dc8d8d903240208a8c885292b666
shamskhodary <shams.khodary@gmail.com>
2022-10-30 23:16:27 +0200

    used cron jobs module to send a reminder email to the user of the upcoming events he joined

---
+++ src/cornJobs/ReminderEmail.ts
@@ -0,0 +1,40 @@
+import corn from 'node-cron'
+import sendEmail from '../helpers/sendingEmail'
+import dayjs from 'dayjs'
+import { Event, JoinedPeople, User } from '../models'
+
+import { Op } from 'sequelize'
+
+corn.schedule('* 15 * * * *', async () => {
+  const now = dayjs().toISOString()
+  const afterOneHour = dayjs().add(1, 'hour').toISOString()
+
+  const eventToStart = await Event.findAll({
+    include: {
+      model: JoinedPeople,
+      include: [{ model: User, attributes: ['email', 'username'] }]
+    },
+    where: {
+      startTime: {
+        [Op.between]: [now, afterOneHour]
+      }
+    }
+  })
+
+  eventToStart.forEach((e: any) => {
+    e.dataValues.JoinedPeople.forEach((ele: any) => {
+      if (ele) {
+        sendEmail(
+          ele.User.email,
+          "Don't miss your events!!",
+          `Hi, Thanks for joining us ${ele.User.username}!!
+
+          The event/s you joined "${e.dataValues.name}" will start in an hour, we would love to seeing you in the event place and hope you enjoy it as much as you can! Don't forget to
+          tell us what you think about it in the event comments!
+          
+          Have fun!`
+        )
+      }
+    })
+  })
+})
