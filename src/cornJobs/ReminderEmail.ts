497aec48b9004a79a45d3bc61f9c870fdc98f458
shamskhodary <shams.khodary@gmail.com>
2022-10-31 10:47:31 +0200

    add a boolean value in the joinedPeople model to check if the email is sent or not

--- src/cornJobs/ReminderEmail.ts
+++ src/cornJobs/ReminderEmail.ts
@@ -6,13 +6,13 @@ import { Event, JoinedPeople, User } from '../models'
 import { Op } from 'sequelize'
 
 const reminderEmail = () => {
-  corn.schedule('* 15 * * * *', async () => {
+  corn.schedule('*/5 * * * *', async () => {
     const now = dayjs().toISOString()
     const afterOneHour = dayjs().add(1, 'hour').toISOString()
-
     const eventToStart = await Event.findAll({
       include: {
         model: JoinedPeople,
+        attributes: ['isEmailSent', 'id'],
         include: [{ model: User, attributes: ['email', 'username'] }]
       },
       where: {
@@ -22,19 +22,31 @@ const reminderEmail = () => {
       }
     })
 
+    const update = async (id:any) => {
+      await JoinedPeople.update({
+        isEmailSent: true
+      },
+      {
+        where: {
+          id
+        }
+      })
+    }
+
     eventToStart.forEach((e: any) => {
       e.dataValues.JoinedPeople.forEach((ele: any) => {
-        if (ele) {
+        if (ele && ele.isEmailSent === false) {
           sendEmail(
             ele.User.email,
             "Don't miss your events!!",
             `Hi, Thanks for joining us ${ele.User.username}!!
-  
+            
             The event/s you joined "${e.dataValues.name}" will start in an hour, we would love to seeing you in the event place and hope you enjoy it as much as you can! Don't forget to
             tell us what you think about it in the event comments!
             
             Have fun!`
           )
+          update(ele.id)
         }
       })
     })
