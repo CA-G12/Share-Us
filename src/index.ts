1214528fe8f310e881776254eb3c7bd77692d58d
SaifHayek <seefhayek4@gmail.com>
2022-10-31 07:47:53 +0200

    chang event status every five minutes using node cron related #79

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import app from './app'
 import config from './config/environment'
+import changeStatus from './cronJobs/changeStatus'
 
 const port = config.port
 
+changeStatus()
 app.listen(port, () => {
   console.log(`Server is running at port http://localhost:${port}`)
 })
