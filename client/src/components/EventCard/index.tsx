7dbcb71c9b1ed35bd3da0f99857442c37ae88dfa
shamskhodary <shams.khodary@gmail.com>
2022-11-11 18:20:20 +0200

    Edit production synchronization & add suggested events in the home page related #120

--- client/src/components/EventCard/index.tsx
+++ client/src/components/EventCard/index.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { FC, useState } from 'react'
 import { toast } from 'react-toastify'
 import { useAuth } from '../../hooks/useAuth'
@@ -14,10 +14,15 @@ const EventCardContainer:FC<CardContainerProps> = ({ allEvents, followerId }) =>
   const isInFollowing = (id:number):boolean => auth.user?.following?.includes(id)
   const isMe = (id:number):boolean => auth.user?.id === id
   const isUserProfile = (id:number):boolean => id === followerId
+  const isHome = auth.user && !followerId
+
   const filteredEvents = (events:IEventDetails[]):IEventDetails[] => events
     .filter((evt) => isInFollowing(evt?.User?.id)
     || isMe(evt?.User?.id)
     || isUserProfile(evt?.User?.id))
+
+  const suggestedUsers = (events:IEventDetails[]):IEventDetails[] => events
+    .filter((e) => !isInFollowing(e?.User.id) && !isMe(e?.User?.id) && !isUserProfile(e?.User.id))
 
   const [deletedId, setDeletedId] = useState<number[]>([])
   const handleDelete = async (id:number):Promise<void> => {
@@ -30,6 +35,38 @@ const EventCardContainer:FC<CardContainerProps> = ({ allEvents, followerId }) =>
 
   return (
     <div className="card-container">
+      {isHome && (
+      <Typography sx={{
+        fontWeight: 600,
+        fontSize: '1.3rem',
+        padding: '0 0 0.5rem 1.5rem',
+      }}
+      >
+        Suggested Events
+      </Typography>
+      )}
+      <Grid container spacing={2}>
+        {auth.user && (suggestedUsers(allEvents).sort(() => 0.5 - Math.random())
+          .slice(0, 4).map((evt:any) => (
+            <Grid item xs={3} key={evt.id}>
+              <EventCard event={evt} handleDelete={handleDelete} />
+            </Grid>
+          )))}
+        {(isHome && !suggestedUsers(allEvents).length)
+         && <Typography sx={{ padding: '0.8rem 0 0 2.5rem' }}>No suggested events</Typography>}
+      </Grid>
+      <br />
+      {auth.user && (
+      <Typography
+        sx={{
+          fontWeight: 600,
+          fontSize: '1.3rem',
+          padding: '0.5rem 0 0.5rem 1.5rem',
+        }}
+      >
+        All Events
+      </Typography>
+      )}
       <Grid container spacing={2}>
         {
         auth.user
--- src/db/build.ts
+++ src/db/build.ts
@@ -2,9 +2,13 @@ import { sequelize, Event, User } from '.'
 import fakeData from './FakeData/fakeData.json'
 import config from '../config/environment'
 export const build = async () => {
-  await sequelize.sync({ force: true })
-  await User.bulkCreate(fakeData.Users)
-  await Event.bulkCreate(fakeData.Events)
+  if (config.nodeEnv !== 'production') {
+    await sequelize.sync({ force: true })
+    await User.bulkCreate(fakeData.Users)
+    await Event.bulkCreate(fakeData.Events)
+  } else {
+    await sequelize.sync({ force: false })
+  }
 }
 
 if (config.nodeEnv !== 'test') {
